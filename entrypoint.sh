6d1c8ad029d1ab8180e99e10ef3f89b5f47a62f5
letssudormrf <letssudormrf@gmail.com>
2017-12-01 17:19:39 +0800

    update entrypoint.sh for enable udp relay

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ METHOD=${METHOD:-"chacha20-ietf-poly1305"}
 PASSWORD=${PASSWORD:-"Shadowsocks"}
 PLUGIN=${PLUGIN:-"obfs-server"}
 PLUGIN_OPTS=${PLUGIN_OPTS:-"obfs=tls;failover=240.0.0.1:443"}
+SS_OPTS=${SS_OPTS:-"-u"}
 
-ss-server -s ${SERVER} -p ${PORT} -m ${METHOD} -k ${PASSWORD} --plugin ${PLUGIN} --plugin-opts ${PLUGIN_OPTS}
+ss-server -s ${SERVER} -p ${PORT} -m ${METHOD} -k ${PASSWORD} --plugin ${PLUGIN} --plugin-opts ${PLUGIN_OPTS} ${SS_OPTS}
