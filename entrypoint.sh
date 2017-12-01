140a5fb09b109a08477de6607ab20d64243b3921
letssudormrf <letssudormrf@gmail.com>
2017-12-01 17:00:50 +0800

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ PORT=${PORT:-"8443"}
 METHOD=${METHOD:-"chacha20-ietf-poly1305"}
 PASSWORD=${PASSWORD:-"Shadowsocks"}
 PLUGIN=${PLUGIN:-"obfs-server"}
-PLUGIN_OPTS=${PLUGIN_OPTS:-"obfs=tls;failover=www.redhat.com:443"}
+PLUGIN_OPTS=${PLUGIN_OPTS:-"obfs=tls;failover=240.0.0.1:443"}
 
 ss-server -s ${SERVER} -p ${PORT} -m ${METHOD} -k ${PASSWORD} --plugin ${PLUGIN} --plugin-opts ${PLUGIN_OPTS}
