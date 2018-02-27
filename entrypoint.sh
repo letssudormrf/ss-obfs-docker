45171ef4fef4baa6f3d254e4dde265c6c2dc3d4f
letssudormrf <letssudormrf@gmail.com>
2018-02-28 00:30:08 +0800

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,13 +39,29 @@ localnet 192.168.0.0/255.255.0.0
 ${PROXYLIST}
 EOF
 nohup ${PROXYCHAINS} ${BIN} ${OPTS} >> /dev/stdout 2>&1 &
-else
-nohup ${BIN} ${OPTS} >> /dev/stdout 2>&1 &
-fi
-
 while sleep 60; do
 if [ "${PROXYLIST_IP}" != "$(getent hosts ${PROXYLIST_STR2} | awk '{print $1}')" ]; then
+PROXYLIST_IP=$(getent hosts ${PROXYLIST_STR2} | awk '{print $1}')
+PROXYLIST="${PROXYLIST_STR1} ${PROXYLIST_IP} ${PROXYLIST_STR3}"
+if [ -n "${PROXYDNS}" ]; then
+proxy_dns="proxy_dns"
+fi
+if [ -n "${PROXY}" ]; then
+cat > ${PROXYCHAINS_CONF} <<EOF
+strict_chain
+quiet_mode
+${proxy_dns}
+localnet 127.0.0.0/255.0.0.0
+localnet 10.0.0.0/255.0.0.0
+localnet 172.16.0.0/255.240.0.0
+localnet 192.168.0.0/255.255.0.0
+[ProxyList]
+${PROXYLIST}
+EOF
 pkill ${BIN}
 nohup ${PROXYCHAINS} ${BIN} ${OPTS} >> /dev/stdout 2>&1 &
 fi
 done
+else
+${BIN} ${OPTS}
+fi
